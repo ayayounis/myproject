9eae576847e241b5c2bf0d62d1f9711cdc5a7d2c
ayayounis <ayayounis@gmail.com>
2018-03-22 23:18:46 +0300

    final update

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'DriversController@index');
 
 //Restaurants
 Route::get('add/restaurant', 'RestaurantController@create')->name('restaurant.create');
